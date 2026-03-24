9825450ce651f37c5ceecedf36d4e2b23c6f132c
Denny Mathew <devdenmathew@gmail.com>
2026-03-24 16:00:21 +0100

    Adds schema

--- RemindersGRDB/Schema.swift
+++ RemindersGRDB/Schema.swift
@@ -2,24 +2,38 @@ import Foundation
 import SQLiteData
 
 @Table
-struct RemindersList {
+struct RemindersList: Identifiable {
     let id: Int
     var color = 0x4a99ef_ff
     var title = ""
 }
 
 @Table
-struct Tag {
+struct Tag: Identifiable {
     let id: Int
     var title = ""
 }
 
 @Table
-struct Reminder {
+struct Reminder: Identifiable {
     let id: Int
     var dueDate: Date?
     var isCompleted = false
     var isFlagging = false
     var notes = ""
+    var priority: Priority?
+    var remindersListID: RemindersList.ID
     var title = ""
+
+    enum Priority: Int, QueryBindable {
+        case low = 1
+        case medium
+        case high
+    }
+}
+
+@Table
+struct ReminderTag {
+    let reminderID: Reminder.ID
+    let tagID: Tag.ID
 }
