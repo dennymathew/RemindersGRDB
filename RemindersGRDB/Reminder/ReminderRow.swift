ba8996b3cb98e3a5638ea0ca81122754620fadf5
Denny Mathew <devdenmathew@gmail.com>
2026-03-30 18:01:43 +0200

    Adds Reminders

---
+++ RemindersGRDB/Reminder/ReminderRow.swift
@@ -0,0 +1,152 @@
+import SQLiteData
+import SwiftUI
+
+struct ReminderRow: View {
+    let color: Color
+    let isPastDue: Bool
+    let reminder: Reminder
+    let tags: [String]
+    let onDetailsTapped: () -> Void
+
+    var body: some View {
+        HStack {
+            HStack(alignment: .firstTextBaseline) {
+                Button {} label: {
+                    Image(
+                        systemName: reminder.isCompleted
+                        ? "circle.inset.filled"
+                        : "circle"
+                    )
+                    .foregroundStyle(.gray)
+                    .font(.title2)
+                    .padding(.trailing, 5)
+                }
+
+                VStack(alignment: .leading) {
+                    HStack(alignment: .firstTextBaseline) {
+                        if let priority = reminder.priority {
+                            Text(
+                                String(
+                                    repeating: "!",
+                                    count: priority.rawValue
+                                )
+                            )
+                            .foregroundStyle(
+                                reminder.isCompleted
+                                ? .gray : color
+                            )
+                        }
+
+                        Text(reminder.title)
+                            .foregroundStyle(
+                                reminder.isCompleted
+                                ? .gray : .primary
+                            )
+                    }
+                    .font(.title3)
+
+                    if !reminder.notes.isEmpty {
+                        Text(
+                            reminder
+                                .notes
+                                .replacingOccurrences(
+                                    of: "\n",
+                                    with: " "
+                                ),
+
+                        )
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                        .lineLimit(2)
+                    }
+
+                    subtitleText
+                }
+            }
+
+            Spacer()
+
+            if !reminder.isCompleted {
+                HStack {
+                    if reminder.isFlagged {
+                        Image(systemName: "flag.fill")
+                            .foregroundStyle(.orange)
+                    }
+
+                    Button {
+                        onDetailsTapped()
+                    } label: {
+                        Image(systemName: "info.circle")
+                    }
+                    .tint(color)
+                }
+            }
+        }
+        .buttonStyle(.borderless)
+        .swipeActions {
+            Button("Delete", role: .destructive) {
+                    // Delete action
+            }
+            Button(reminder.isFlagged ? "Unflag" : "Flag") {
+                    // Toggle flag action
+            }
+            .tint(.orange)
+            Button("Details") {
+                onDetailsTapped()
+            }
+        }
+    }
+
+    private var dueText: Text {
+        if let date = reminder.dueDate {
+            Text(date.formatted(date: .numeric, time: .shortened))
+                .foregroundStyle(isPastDue ? .red : .gray)
+        } else {
+            Text("")
+        }
+    }
+
+    private var subtitleText: Text {
+        let tagsText = tags.reduce(Text("")) { result, tag in
+            result + Text(" #\(tag)")
+        }
+        return
+        (dueText + tagsText.foregroundStyle(.gray).bold())
+            .font(.callout)
+    }
+}
+
+struct ReminderRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let _ = prepareDependencies {
+            $0.defaultDatabase = try! appDatabase()
+        }
+        Inner()
+    }
+
+    struct Inner: View {
+        @FetchAll var reminders: [Reminder]
+        var body: some View {
+            NavigationStack {
+                List {
+                    ForEach(reminders) { reminder in
+                        ReminderRow(
+                            color: .red,
+                            isPastDue: false,
+                            reminder: .init(
+                                id: reminder.id,
+                                dueDate: reminder.dueDate,
+                                isCompleted: reminder.isCompleted,
+                                notes: reminder.notes,
+                                priority: reminder.priority,
+                                remindersListID: reminder.remindersListID,
+                                title: reminder.title,
+                            ),
+                            tags: ["weekend", "fun"]
+                        ) {}
+                    }
+                }
+            }
+        }
+    }
+}
