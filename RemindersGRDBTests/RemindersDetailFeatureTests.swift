aad6bcf3459fe17e8fe58dabf855ff20bdb05398
Denny Mathew <devdenmathew@gmail.com>
2026-04-17 15:26:15 +0200

    Fixes tests

--- RemindersGRDBTests/RemindersDetailFeatureTests.swift
+++ RemindersGRDBTests/RemindersDetailFeatureTests.swift
@@ -14,107 +14,171 @@ extension BaseTestSuite {
             }
             let model = RemindersDetailModel(detailType: .remindersList(remindersList))
 
-            try await model.$reminders.load()
-            assertInlineSnapshot(of: model.reminders, as: .customDump) {
+            try await model.$rows.load()
+            assertInlineSnapshot(of: model.$rows, as: .customDump) {
                 """
                 [
-                  [0]: Reminder(
-                    id: 6,
-                    dueDate: Date(2023-11-16T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: true,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Pick up kids from school"
+                  [0]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 6,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-16T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: true,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Pick up kids from school",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   ),
-                  [1]: Reminder(
-                    id: 8,
-                    dueDate: Date(2023-11-18T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: false,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Take out trash"
+                  [1]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 8,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-18T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: false,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Take out trash",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   )
                 ]
                 """
             }
 
             await model.toggleShowCompletedButtonTapped()
-            try await model.$reminders.load()
-            assertInlineSnapshot(of: model.reminders, as: .customDump) {
+            try await model.$rows.load()
+            assertInlineSnapshot(of: model.rows, as: .customDump) {
                 """
                 [
-                  [0]: Reminder(
-                    id: 6,
-                    dueDate: Date(2023-11-16T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: true,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Pick up kids from school"
+                  [0]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 6,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-16T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: true,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Pick up kids from school",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   ),
-                  [1]: Reminder(
-                    id: 8,
-                    dueDate: Date(2023-11-18T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: false,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Take out trash"
+                  [1]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 8,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-18T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: false,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Take out trash",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   ),
-                  [2]: Reminder(
-                    id: 7,
-                    dueDate: Date(2023-11-12T22:13:20.000Z),
-                    isCompleted: true,
-                    isFlagged: false,
-                    notes: "",
-                    priority: .low,
-                    remindersListID: 2,
-                    title: "Get laundry"
+                  [2]: RemindersDetailModel.Row(
+                    isPastDue: false,
+                    reminder: Reminder(
+                      id: 7,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-12T22:13:20.000Z),
+                      isCompleted: true,
+                      isFlagged: false,
+                      notes: "",
+                      priority: .low,
+                      remindersListID: 2,
+                      title: "Get laundry",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   )
                 ]
                 """
             }
 
             await model.orderingButtonTapped(.priority)
-            try await model.$reminders.load()
-            assertInlineSnapshot(of: model.reminders, as: .customDump) {
+            try await model.$rows.load()
+            assertInlineSnapshot(of: model.rows, as: .customDump) {
                 """
                 [
-                  [0]: Reminder(
-                    id: 6,
-                    dueDate: Date(2023-11-16T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: true,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Pick up kids from school"
+                  [0]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 6,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-16T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: true,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Pick up kids from school",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   ),
-                  [1]: Reminder(
-                    id: 8,
-                    dueDate: Date(2023-11-18T22:13:20.000Z),
-                    isCompleted: false,
-                    isFlagged: false,
-                    notes: "",
-                    priority: .high,
-                    remindersListID: 2,
-                    title: "Take out trash"
+                  [1]: RemindersDetailModel.Row(
+                    isPastDue: true,
+                    reminder: Reminder(
+                      id: 8,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-18T22:13:20.000Z),
+                      isCompleted: false,
+                      isFlagged: false,
+                      notes: "",
+                      priority: .high,
+                      remindersListID: 2,
+                      title: "Take out trash",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   ),
-                  [2]: Reminder(
-                    id: 7,
-                    dueDate: Date(2023-11-12T22:13:20.000Z),
-                    isCompleted: true,
-                    isFlagged: false,
-                    notes: "",
-                    priority: .low,
-                    remindersListID: 2,
-                    title: "Get laundry"
+                  [2]: RemindersDetailModel.Row(
+                    isPastDue: false,
+                    reminder: Reminder(
+                      id: 7,
+                      createdAt: nil,
+                      dueDate: Date(2023-11-12T22:13:20.000Z),
+                      isCompleted: true,
+                      isFlagged: false,
+                      notes: "",
+                      priority: .low,
+                      remindersListID: 2,
+                      title: "Get laundry",
+                      updatedAt: nil
+                    ),
+                    tags: [
+                      [0]: "easy-win"
+                    ]
                   )
                 ]
                 """
